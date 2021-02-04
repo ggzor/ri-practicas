5c8121facc3c2b36abdcc196b36c2a270dc7790d
ggzor <30713864+ggzor@users.noreply.github.com>
2021-02-03 22:23:26 -0600

    Agregando comentarios para la práctica 3

--- Practica3/main.py
+++ Practica3/main.py
@@ -1,21 +1,42 @@
 from re import compile
 from itertools import count
 
+# Expresión regular para una palabra que no empieza con un
+# un signo de dolar
 re_palabra = compile(r"(?<!\$)\b[a-zñáéíóú]+\b")
 
 palabras = set()
 matriz = []
 
 with open("SalidaSinCerradas.txt") as archivo:
+    # Agregar las palabras nuevas al conjunto de palabras
     for linea in archivo:
         palabras.update(re_palabra.findall(linea))
+
+    # Ordenar las palabras alfabéticamente
     palabras = sorted(palabras)
+
+    # Crear un índice invertido
+    # palabra -> índice
     indice_palabras = {p: i for p, i in zip(palabras, count())}
+
     archivo.seek(0)
+
     for linea in archivo:
+        # Agregar una fila de ceros a la matriz para el documento actual
         matriz.append([0] * len(palabras))
+
+        # Registrar la presencia de cada palabra en la matriz
         for palabra in re_palabra.findall(linea):
             matriz[-1][indice_palabras[palabra]] = 1
+
+with open("palabras.txt", "w") as archivo_palabras:
+    archivo_palabras.write("\n".join(palabras))
+
+with open("matriz.txt", "w") as archivo_matriz:
+    # Imprimir cada fila de cada documento
+    for fila in matriz:
+        archivo_matriz.write(f"{','.join(str(x) for x in fila)}\n")
 
 consultas = [
     ["mejor"],
@@ -27,13 +48,17 @@ consultas = [
 ]
 
 for consulta in consultas:
+    # Sí alguna de las palabras no se encuentra en el índice de palabras
+    # abortar la búsqueda
     if any(p not in indice_palabras for p in consulta):
         print(",".join(consulta), ":")
         continue
 
+    # Encontrar los índices de los documentos relevantes
     documentos_relevantes = []
     for d, indice_doc in zip(matriz, count()):
         if all(matriz[indice_doc][indice_palabras[p]] == 1 for p in consulta):
             documentos_relevantes.append(indice_doc + 1)
 
+    # Imprimir el resultado de la consulta
     print(",".join(consulta), ":", ",".join(str(dr) for dr in documentos_relevantes))
