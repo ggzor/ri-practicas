a76f96349ba9d5f009323a483324996a0eb53d3f
ggzor <30713864+ggzor@users.noreply.github.com>
2021-04-27 23:51:32 -0500

    Agregando consultas para el segundo parcial

--- SegundoParcial/main.py
+++ SegundoParcial/main.py
@@ -94,19 +94,70 @@ with open(originales, encoding="utf-8") as archivo:
             vocabulario.add(palabra)
             conteo_palabras[palabra][i] += 1
 
+    Path("NoticiasProcesadas.txt").write_text(
+        "\n".join(f"noticia{i} $ {' '.join(d)}" for i, d in enumerate(documentos, 1)),
+        encoding="utf-8",
+    )
+
     vocabulario = sorted(vocabulario)
+    Path("vocabulario.txt").write_text("\n".join(vocabulario), encoding="utf-8")
+    print("Longitud del vocabulario: ", len(vocabulario))
 
     # Frecuencias de cada termino por documento
     frecuencias = {p: [v[i] for i in range(N)] for p, v in conteo_palabras.items()}
 
     # Pesos TF por documento
     tf = {p: [pesoTF(x) for x in l] for p, l in frecuencias.items()}
+    Path("tf.csv").write_text(
+        "\n".join(f"{p},{','.join(str(x) for x in tf[p])}" for p in sorted(tf.keys())),
+        encoding="utf-8",
+    )
 
     # Cantidad de documentos con el término
     ni = {p: len([x for x in l if x > 0]) for p, l in frecuencias.items()}
 
     # Frecuencia inversa por término
     idf = {p: log(N / x, 2) for p, x in ni.items()}
+    Path("idf.csv").write_text(
+        "\n".join(f"{p},{idf[p]}" for p in sorted(idf.keys())), encoding="utf-8"
+    )
 
     # Pesos finales por cada término
     wi = {p: [x * idf[p] for x in l] for p, l in tf.items()}
+    Path("representacion_documentos.csv").write_text(
+        "\n".join(
+            f"noticia{i + 1},{','.join(str(wi[p][i]) for p in sorted(wi.keys()))}"
+            for i in range(N)
+        ),
+        encoding="utf-8",
+    )
+
+    # Vectores normalizados
+    vector = {d: (sum(l[d] ** 2 for _, l in wi.items())) ** 0.5 for d in range(0, N)}
+
+    # Función para rankear los documentos
+    def rankear(consulta):
+        resultado = {}
+
+        for d in range(N):
+            suma = 0
+            for t in consulta:
+                suma += idf.get(t, 0) * wi.get(t, [0] * N)[d]
+            if suma > 0:
+                resultado[d + 1] = suma / vector[d]
+
+        return sorted(resultado.items(), key=lambda r: r[1], reverse=True)
+
+    consultas = [
+        ["hoy", "telecomunicaciones"],
+        ["televisoras", "país", "servicios"],
+        ["a"],
+        ["narcotráfico", "chapo"],
+        ["putin", "rusia"],
+    ]
+
+    # Realizar las consultas
+    for c in consultas:
+        print("Consulta: ", ",".join(c))
+        pprint(rankear(c))
+        print()
